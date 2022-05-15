60e638e7feb9892cde257facee8e4437272f3493
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2022-05-15 19:16:26 +0300

    Add alert component: AppAlert

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,41 @@
+<template>
+    <sl-alert>
+        <sl-icon slot="icon" :name="icon"></sl-icon>
+
+        <strong v-if="title" class="block">{{ title }}</strong>
+
+        <slot></slot>
+    </sl-alert>
+</template>
+
+<script setup>
+import '@shoelace-style/shoelace/dist/components/alert/alert.js'
+import '@shoelace-style/shoelace/dist/components/icon/icon.js'
+import { computed, useAttrs } from 'vue'
+
+defineProps({
+    title: {
+        type: String,
+        required: false,
+    }
+})
+
+const attrs = useAttrs()
+
+const icon = computed(() => {
+    switch (attrs.variant) {
+        case 'primary':
+            return 'info-circle'
+        case 'neutral':
+            return 'gear'
+        case 'success':
+            return 'check2-circle'
+        case 'warning':
+            return 'exclamation-triangle'
+        case 'danger':
+            return 'exclamation-octagon'
+    }
+
+    return 'info-circle'
+})
+</script>
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -2,23 +2,38 @@
     <h1 class="text-2xl text-center py-4">Sign Up</h1>
 
     <div class="max-w-sm mx-auto my-4">
-        <sl-alert variant="success" open v-show="success">
-            <sl-icon slot="icon" name="check2-circle"></sl-icon>
-            <strong>Success!</strong><br />
+        <AppAlert variant="success" title="Success!" v-show="success" open closable>
             You have been signed up successfully.
-        </sl-alert>
+        </AppAlert>
 
-        <sl-alert variant="danger" open v-show="failure">
-            <sl-icon slot="icon" name="exclamation-octagon"></sl-icon>
-            <strong>Failure</strong><br />
+        <AppAlert variant="danger" title="Failure!" v-show="failure" open closable>
             Some problem occuried.
-        </sl-alert>
+        </AppAlert>
     </div>
 
     <form @submit.prevent="submit" class="max-w-sm mx-auto">
-        <sl-input name="name" label="Your Name" type="text" class="my-4"></sl-input>
-        <sl-input name="email" label="Email" type="email" class="my-4"></sl-input>
-        <sl-input name="password" label="Password" type="password" class="my-4" toggle-password></sl-input>
+        <sl-input
+            name="name"
+            type="text"
+            label="Your Name"
+            class="my-4"
+        ></sl-input>
+
+        <sl-input
+            name="email"
+            type="email"
+            label="Email"
+            class="my-4"
+        ></sl-input>
+
+        <sl-input
+            name="password"
+            type="password"
+            label="Password"
+            class="my-4"
+            minlength="6"
+            toggle-password
+        ></sl-input>
 
         <div class="text-center my-6">
             <sl-button variant="primary" type="submit" pill>Sign Up</sl-button>
@@ -32,10 +47,10 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
 import '@shoelace-style/shoelace/dist/components/input/input.js'
 import '@shoelace-style/shoelace/dist/components/button/button.js'
-import '@shoelace-style/shoelace/dist/components/alert/alert.js'
+import { ref } from 'vue'
+import AppAlert from '@/components/AppAlert.vue'
 import supabase from '@/supabase.js'
 
 const success = ref(false)
