bce42895335394ffd202a22c40db73ea8271af2e
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2022-05-16 22:49:17 +0300

    Tweak chat design

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,25 +1,12 @@
 <template>
-    <PageHeader>Welcome!</PageHeader>
-
-    <div class="max-w-sm mx-auto">
-        <div v-if="!isAuthenticated">
-            <p class="text-center">
-                Please, <router-link :to="{ name: 'SignIn' }">Sign In</router-link> to enter the chat.
-            </p>
-        </div>
-        <div v-else>
-            <p class="text-center">
-                You are signed in as <strong class="text-bold-800">{{ user.email }}</strong>.
-
-                <button type="button" @click="signOut" class="link">Sign Out</button>
-            </p>
-        </div>
+    <div class="sm:flex pb-2">
+        <p class="flex-1 mb-1">You are signed in as <strong class="text-bold-800">{{ user.user_metadata.username }}</strong></p>
+        <button type="button" @click="signOut" class="link">Sign Out</button>
     </div>
 </template>
 
 <script setup>
-import PageHeader from '@/components/PageHeader.vue'
-import { user, isAuthenticated, useSignOut } from '@/composables/useAuth.js'
+import { user, useSignOut } from '@/composables/useAuth.js'
 
 const { signOut } = useSignOut()
 </script>
