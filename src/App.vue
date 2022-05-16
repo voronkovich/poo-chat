3a3bbc647e589d68471871cb5f355b4957bd454c
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2022-05-16 03:07:16 +0300

    Tweak app for small devices

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <template>
-    <header class="max-w-screen-sm mx-auto p-4 mt-16 text-3xl text-center">
+    <header class="max-w-screen-sm mx-auto p-4 mt-2 md:mt-16 text-3xl text-center">
         <router-link :to="{ name: 'Chat' }">SupaChat</router-link>
     </header>
 
-    <div class="max-w-screen-sm mx-auto bg-white py-8 mt-2 rounded-2xl shadow">
+    <div class="max-w-screen-sm mx-auto bg-white py-8 px-6 min-h-full mt-2 rounded-2xl shadow">
         <main class="max-w-screen-md mx-auto">
             <router-view></router-view>
         </main>
     </div>
 </template>
-
-<script setup>
-</script>
