662b174e3756323bd967a57c21cdd52befb2eee8
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2022-05-17 03:04:18 +0300

    Add message form component

---
+++ src/components/ChatMessageForm.vue
@@ -0,0 +1,33 @@
+<template>
+    <form>
+        <textarea
+            class="block w-full h-20 form-control"
+            placeholder="Enter message here..."
+            v-model="message"
+            @keypress.enter="submit"
+        ></textarea>
+    </form>
+</template>
+
+<script setup>
+import supabase from '@/services/supabase.js'
+import { ref } from 'vue'
+
+const message = ref(null)
+const user = supabase.auth.user()
+
+const submit = async ({ target: form }) => {
+    const { data, error } = await supabase
+        .from('messages')
+        .insert({
+            user_id: user.id,
+            content: message.value,
+        })
+
+    if (error) {
+        return
+    }
+
+    message.value = ''
+}
+</script>
--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -3,10 +3,13 @@
         <p class="flex-1 mb-1">You are signed in as <strong class="text-bold-800">{{ user.user_metadata.username }}</strong></p>
         <button type="button" @click="signOut" class="link">Sign Out</button>
     </div>
+
+    <ChatMessageForm></ChatMessageForm>
 </template>
 
 <script setup>
 import { user, useSignOut } from '@/composables/useAuth.js'
+import ChatMessageForm from '@/components/ChatMessageForm.vue'
 
 const { signOut } = useSignOut()
 </script>
