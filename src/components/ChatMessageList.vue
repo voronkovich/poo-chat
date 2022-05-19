d755a4667151b94c95ad98daa8f54246718f2a4a
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2022-05-19 06:04:55 +0300

    Automatically scroll to last added message

--- src/components/ChatMessageList.vue
+++ src/components/ChatMessageList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="h-[50vh] overflow-y-scroll">
+    <div class="h-[50vh] overflow-y-scroll" ref="container">
         <div v-for="message of messages" :key="message.id" class="py-2">
             <Username :username="message.username" />: {{ message.content }}
         </div>
@@ -7,8 +7,19 @@
 </template>
 
 <script setup>
+import { ref, watch, onUpdated, onMounted } from 'vue'
 import useChatMessages from '@/composables/useChatMessages.js'
 import Username from '@/components/Username.vue'
 
 const { messages } = await useChatMessages(50)
+
+const container = ref()
+
+const scrollToLastChatMessage = () => {
+    container.value.lastElementChild.scrollIntoView()
+}
+
+onUpdated(scrollToLastChatMessage)
+
+onMounted(scrollToLastChatMessage)
 </script>
